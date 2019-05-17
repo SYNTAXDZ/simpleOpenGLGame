d2bfb051e0b0232e22758431aaefb0bf4c2d22bb
SYNTAXDZ <miloudiadelhani@gmail.com>
2019-05-17 17:03:57 +0100

    adding the base resources file

---
+++ src/game.cpp
@@ -0,0 +1,16 @@
+#include "game.h"
+#include "resource_manager.h"
+
+// set the default members to the constructor
+Game::Game( unsigned int width, unsigned int height ) 
+                    : State( GAME_ACTIVE ), Keys(), Width( width ), Height( height ) {}
+
+Game::~Game() {}
+
+void Game::Init() {}
+
+void Game::Update( float dt ) {}
+
+void Game::ProcessInput( float dt ) {}
+
+void Game::Render() {}
