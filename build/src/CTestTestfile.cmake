b43b61c3a8473910654c894d359d6ed6e8ea0ecf
SYNTAXDZ <miloudiadelhani@gmail.com>
2019-05-17 17:04:21 +0100

    updating the build dir..

--- build/src/CTestTestfile.cmake
+++ build/src/CTestTestfile.cmake
@@ -1,7 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/syntax/Knowledge/learn_cpp/simpleOpenGLGame
-# Build directory: /home/syntax/Knowledge/learn_cpp/simpleOpenGLGame/build
+# Source directory: /home/syntax/Knowledge/learn_cpp/simpleOpenGLGame/src
+# Build directory: /home/syntax/Knowledge/learn_cpp/simpleOpenGLGame/build/src
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
